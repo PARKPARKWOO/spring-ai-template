d0687eff602c8a019956f61e2e5d7eff5ca96999
PARKPARKWOO <wy9295@naver.com>
2026-03-25 23:39:56 +0900

    GoogleGenAiEmbeddingConnectionAutoConfiguration 무시

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,7 +3,11 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        org.springframework.ai.model.google.genai.autoconfigure.embedding.GoogleGenAiEmbeddingConnectionAutoConfiguration::class,
+    ]
+)
 class DemoApplication
 
 fun main(args: Array<String>) {
